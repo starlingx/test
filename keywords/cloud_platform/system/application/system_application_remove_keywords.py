d17b22d2c29892f5353cf692f9140b9ae952ce4e
croy <Christian.Roy@windriver.com>
2025-01-09 16:18:07 -0500

    Replacing track_status by validate_app_status
    In system_application_list_keyword, replacing track_status
    with validate_app_status to make use of the new validation
    library.
    
    Change-Id: I13718cdaad127c219f9b12f20bedd812613a313e

--- keywords/cloud_platform/system/application/system_application_remove_keywords.py
+++ keywords/cloud_platform/system/application/system_application_remove_keywords.py
@@ -41,20 +41,12 @@ class SystemApplicationRemoveKeywords(BaseKeyword):
         self.validate_success_return_code(self.ssh_connection)
         system_application_output = SystemApplicationOutput(output)
 
-        # Tracks the execution of the command 'system application-apply' until its completion or a timeout.
-
-        # Setups the status tracker. Note: there's no 'removed' status. When an app is removed its status comes back to 'uploaded'.
-        system_application_list_status_tracking_input = SystemApplicationListStatusTrackingInput(app_name, SystemApplicationStatusEnum.UPLOADED)
-        system_application_list_status_tracking_input.set_timeout_in_seconds(system_application_remove_input.get_timeout_in_seconds())
-        system_application_list_status_tracking_input.set_check_interval_in_seconds(system_application_remove_input.get_check_interval_in_seconds())
-
-        # Tracks the status of the application.
+        # Tracks the execution of the command 'system application-remove' until its completion or a timeout.
         system_application_list_keywords = SystemApplicationListKeywords(self.ssh_connection)
-        system_application_list_output = system_application_list_keywords.track_status(system_application_list_status_tracking_input)
+        system_application_list_keywords.validate_app_status(app_name, 'uploaded')
 
         # If the execution arrived here the status of the application is 'uploaded', that is, the application was removed.
-        application = system_application_list_output.get_application(app_name)
-        system_application_output.get_system_application_object().set_status(application.get_status())
+        system_application_output.get_system_application_object().set_status('uploaded')
 
         return system_application_output
 
