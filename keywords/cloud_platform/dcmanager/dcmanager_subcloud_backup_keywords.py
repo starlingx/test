b9cf2add1c47d75fff8d5124238b1597e71ca654
Swapna Gorre <swapna.gorre@windriver.com>
2025-02-10 14:10:10 -0500

    Migrate backup test cases under test_delete_subcloud_backup.py (1/2)
      - migrate backup test cases from old framework to new framework.
      - Add test cases for test_delete_backup_central,
        test_delete_backup_local
      - Implement keywords related to backup operations.
    
    Change-Id: I3d3749951353196ac3e5da433eb0986e6d2e61e3
    Signed-off-by: Swapna Gorre <swapna.gorre@windriver.com>

---
+++ keywords/cloud_platform/dcmanager/dcmanager_subcloud_backup_keywords.py
@@ -0,0 +1,190 @@
+from typing import Optional
+
+from framework.ssh.ssh_connection import SSHConnection
+from framework.validation.validation import validate_equals_with_retry
+from keywords.base_keyword import BaseKeyword
+from keywords.cloud_platform.command_wrappers import source_openrc
+from keywords.files.file_keywords import FileKeywords
+
+
+class DcManagerSubcloudBackupKeywords(BaseKeyword):
+    """
+    This class contains all the keywords related to the 'dcmanager subcloud-backup <create/delete>' command.
+    """
+
+    def __init__(self, ssh_connection: SSHConnection):
+        """
+        Constructor.
+
+        Args:
+            ssh_connection (SSHConnection): SSH connection to the target system.
+        """
+        self.ssh_connection = ssh_connection
+
+    def create_subcloud_backup(
+        self,
+        sysadmin_password: str,
+        con_ssh: SSHConnection,
+        path: str,
+        subcloud: Optional[str] = None,
+        local_only: bool = False,
+        backup_yaml: Optional[str] = None,
+        group: Optional[str] = None,
+        registry: bool = False,
+    ) -> None:
+        """
+        Creates a backup of the specified subcloud.
+
+        Args:
+            sysadmin_password (str): Subcloud sysadmin password needed for backup creation.
+            con_ssh (SSHConnection): SSH connection to execute the command (central_ssh or subcloud_ssh).
+            path (str): The directory path where the backup file will be checked.
+            subcloud (Optional[str]): The name of the subcloud to backup. Defaults to None.
+            local_only (bool): If True, backup will be stored only in the subcloud. Defaults to False.
+            backup_yaml (Optional[str]): path to use the yaml file. Defaults to None.
+            group (Optional[str]): Subcloud group name to create backup. Defaults to None.
+            registry (bool): Option to add the registry backup in the same task. Defaults to False.
+
+        Returns:
+            None:
+        """
+        # Command construction
+        cmd = (
+            f"dcmanager subcloud-backup create --sysadmin-password {sysadmin_password}"
+        )
+        if subcloud:
+            cmd += f" --subcloud {subcloud}"
+        if local_only:
+            cmd += " --local-only"
+        if backup_yaml:
+            cmd += f" --backup-values {backup_yaml}"
+        if group:
+            cmd += f" --group {group}"
+        if registry:
+            cmd += " --registry-images"
+
+        self.ssh_connection.send(source_openrc(cmd))
+        self.validate_success_return_code(self.ssh_connection)
+
+        # Use wait_for_backup_creation to ensure the file is created
+        self.wait_for_backup_creation(con_ssh, path, subcloud)
+
+    def wait_for_backup_creation(
+        self,
+        con_ssh: SSHConnection,
+        path: str,
+        subcloud: Optional[str],
+        check_interval: int = 30,
+        timeout: int = 600,
+    ) -> None:
+        """
+        Waits for the backup file to be created in the specified path.
+
+        Args:
+            con_ssh (SSHConnection): SSH connection to execute the command (central_ssh or subcloud_ssh).
+            path (str): The path where the backup file is expected.
+            subcloud (Optional[str]): The name of the subcloud to check.
+            check_interval (int): Time interval (in seconds) to check for file creation. Defaults to 30.
+            timeout (int): Maximum time (in seconds) to wait for file creation. Defaults to 600.
+
+        Returns:
+            None:
+        """
+
+        def check_backup_created() -> str:
+            """
+            Checks if the backup has been created.
+
+            Returns:
+                str: A message indicating whether the backup has been successfully created or not.
+            """
+            check_file = FileKeywords(con_ssh).validate_file_exists_with_sudo(path)
+            if check_file:
+                return f"Backup should be created at {path}"
+            else:
+                return "Backup not created yet."
+
+        validate_equals_with_retry(
+            function_to_execute=check_backup_created,
+            expected_value=f"Backup should be created at {path}",
+            validation_description=f"Backup creation for subcloud {subcloud} completed.",
+            timeout=timeout,
+            polling_sleep_time=check_interval,
+        )
+
+    def delete_subcloud_backup(
+        self,
+        con_ssh: SSHConnection,
+        path: str,
+        release: str,
+        subcloud: Optional[str] = None,
+        local_only: bool = False,
+        group: Optional[str] = None,
+        sysadmin_password: str = None,
+    ) -> None:
+        """
+        Sends the command to delete the backup of the specified subcloud and waits for confirmation of its deletion.
+
+        Args:
+            con_ssh (SSHConnection): SSH connection to execute the command (central_ssh or subcloud_ssh).
+            path (str): The path where the backup file is located.
+            release (str): Required to delete a release backup.
+            subcloud (Optional[str]): The name of the subcloud to delete the backup. Defaults to None.
+            local_only (bool): If True, only deletes the local backup in the subcloud. Defaults to False.
+            group (Optional[str]): Subcloud group name to delete backup. Defaults to None.
+            sysadmin_password (str): Subcloud sysadmin password needed for deletion on local_path. Defaults to None.
+
+        Returns:
+            None:
+        """
+        # Command construction for backup deletion
+        cmd = f"dcmanager subcloud-backup delete {release}"
+        if subcloud:
+            cmd += f" --subcloud {subcloud}"
+        if local_only:
+            cmd += " --local-only"
+        if group:
+            cmd += f" --group {group}"
+        if sysadmin_password:
+            cmd += f" --sysadmin-password {sysadmin_password}"
+
+        self.ssh_connection.send(source_openrc(cmd))
+        self.validate_success_return_code(self.ssh_connection)
+
+        # Call wait_for_backup_deletion method to wait and verify the backup deletion.
+        self.wait_for_backup_deletion(con_ssh, path, subcloud)
+
+    def wait_for_backup_deletion(
+        self, con_ssh: SSHConnection, path: str, subcloud: str
+    ) -> None:
+        """
+        Waits for the backup to be deleted by checking for the absence of the backup file.
+
+        Args:
+            con_ssh (SSHConnection): SSH connection object to execute the command.
+            path (str): The path where the backup file was located.
+            subcloud (str): The name of the subcloud to delete the backup.
+
+        Returns:
+            None:
+        """
+
+        def check_backup_deleted() -> str:
+            """
+            Checks if the backup has been deleted.
+
+            Returns:
+                str: Confirmation message if the backup is deleted, otherwise an error message.
+            """
+            check_file = FileKeywords(con_ssh).validate_file_exists_with_sudo(path)
+            if not check_file:
+                return f"Backup successfully deleted from {path} for {subcloud}"
+            else:
+                return f"Backup still exists at {path}."
+
+        # Using validate_equals_with_retry to ensure the backup is deleted.
+        validate_equals_with_retry(
+            function_to_execute=check_backup_deleted,
+            expected_value=f"Backup successfully deleted from {path} for {subcloud}",
+            validation_description=f"Backup deletion for subcloud {subcloud} completed.",
+        )
