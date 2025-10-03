3b9b74c948517ad6bcab8171755c6ac0c1597af6
Lucas Wizer da Silva <lucas.wizerdasilva@windriver.com>
2025-10-03 16:26:25 -0300

    Retry swact command if the host is not ready
    Change-Id: I40f2be4d12258eb600e9b23b2869fbaa18f40650
    Signed-off-by: Lucas Wizer da Silva <lucas.wizerdasilva@windriver.com>

--- keywords/cloud_platform/system/host/system_host_swact_keywords.py
+++ keywords/cloud_platform/system/host/system_host_swact_keywords.py
@@ -2,6 +2,7 @@ import time
 
 from framework.exceptions.keyword_exception import KeywordException
 from framework.logging.automation_logger import get_logger
+from framework.ssh.ssh_connection import SSHConnection
 from keywords.base_keyword import BaseKeyword
 from keywords.cloud_platform.command_wrappers import source_openrc
 from keywords.cloud_platform.system.host.objects.system_host_object import SystemHostObject
@@ -13,17 +14,21 @@ class SystemHostSwactKeywords(BaseKeyword):
     System Host Swact Keywords
     """
 
-    def __init__(self, ssh_connection):
+    def __init__(self, ssh_connection: SSHConnection):
         """
         Constructor
+
         Args:
-            ssh_connection:
+            ssh_connection (SSHConnection): SSH connection to use.
         """
         self.ssh_connection = ssh_connection
 
-    def host_swact(self) -> bool:
+    def host_swact(self, swact_accepted_timeout: int = 300) -> bool:
         """
         Performs a controller switchover action (swact).
+
+        Args:
+            swact_accepted_timeout (int): timeout trying the swact
 
         Returns:
             bool: True if swact was successful, raises exception otherwise.
@@ -31,6 +36,20 @@ class SystemHostSwactKeywords(BaseKeyword):
         active_controller = SystemHostListKeywords(self.ssh_connection).get_active_controller()
         standby_controller = SystemHostListKeywords(self.ssh_connection).get_standby_controller()
         self.ssh_connection.send(source_openrc(f"system host-swact {active_controller.get_host_name()}"))
+
+        # Checking whether the host can be swacted; if not, the process will retry until the timeout is reached.
+        start = time.time()
+        while time.time() - start < swact_accepted_timeout:
+            if self.ssh_connection.get_return_code() == 1:
+                get_logger().log_info("Fail to swact, trying again in 5 seconds")
+                time.sleep(5)
+                self.ssh_connection.send(source_openrc(f"system host-swact {active_controller.get_host_name()}"))
+            else:
+                get_logger().log_info(f"The swact of host {active_controller} to {standby_controller} was started")
+                break
+        else:
+            raise KeywordException(f"Timeout: failed to swact host {active_controller}")
+
         self.validate_success_return_code(self.ssh_connection)
         is_swact_success = self.wait_for_swact(active_controller, standby_controller)
         if not is_swact_success:
@@ -62,16 +81,17 @@ class SystemHostSwactKeywords(BaseKeyword):
         current_active_controller: SystemHostObject,
         current_standby_controller: SystemHostObject,
         swact_timeout: int = 1800,
-    ):
+    ) -> bool:
         """
-        Waits for the swact action to complete by ensuring the controllers switch active -> standby and standby -> active
+        Waits for the swact action to complete by ensuring the controllers switch active -> standby and standby -> active.
+
         Args:
-            current_active_controller (): the current active controller
-            current_standby_controller (): the current standby controller
-            swact_timeout ():
+            current_active_controller (SystemHostObject): The current active controller.
+            current_standby_controller (SystemHostObject): The current standby controller.
+            swact_timeout (int): Timeout in seconds to wait for swact completion.
 
         Returns:
-
+            bool: True if swact completed successfully, False otherwise.
         """
         timeout = time.time() + swact_timeout
         refresh_time = 5
