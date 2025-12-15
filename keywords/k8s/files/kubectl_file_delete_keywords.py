16786d42729285c5118264a26e1c87ee85e88ff7
Dario Oliveira <Dario.DeOliveiraFilho@windriver.com>
2025-12-15 17:22:08 -0300

    Refactored and improved the code for Dell storage scripts.
    Updated the iscsi credentials
    
    Change-Id: Ic22ee7a4c63378f26febfd88fe8bed5702f751dd
    Signed-off-by: Dario Oliveira <Dario.DeOliveiraFilho@windriver.com>

--- keywords/k8s/files/kubectl_file_delete_keywords.py
+++ keywords/k8s/files/kubectl_file_delete_keywords.py
@@ -17,22 +17,27 @@ class KubectlFileDeleteKeywords(BaseKeyword):
         """
         self.ssh_connection = ssh_connection
 
-    def delete_resources(self, file_path: str, validate: bool = True) -> str:
+
+    def delete_resources(self, file_path: str, ignore_not_found: bool = False, validate: bool = True) -> str:
+
         """
         Deletes the dashboard resources
 
         Args:
             file_path (str): the file path
             validate (bool): Enable validation. Defaults to True.
-
+            ignore_not_found (bool): whether to ignore not found errors
         Returns:
             str: the output
-
         """
+
         cmd = f"kubectl delete -f {file_path}"
 
         if not validate:
             cmd += " --validate=false"
+
+        if ignore_not_found:
+            cmd += " --ignore-not-found=true"
 
         output = self.ssh_connection.send(export_k8s_config(cmd))
         self.validate_success_return_code(self.ssh_connection)
