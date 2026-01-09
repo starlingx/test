1b28e0aa29ef1e6cd60fe88ca20fa670fa6b31dc
Giovane Ribeiro Anhaia <giovane.ribeiroanhaia@windriver.com>
2026-01-09 11:38:02 -0300

    Prestaged Subcloud Remote Restore Inactive Load
    Adds new test for remote backup a prestaged subcloud with inactive
    load. The test does the prestage of a subcloud then creates a central
    backup and finally restores it.
    
    Change-Id: I87c9ce4afeac90a0d678445094d07804f33c2197
    Signed-off-by: Giovane Ribeiro Anhaia <giovane.ribeiroanhaia@windriver.com>

--- testcases/cloud_platform/regression/dc/backup_restore/test_backup_restore.py
+++ testcases/cloud_platform/regression/dc/backup_restore/test_backup_restore.py
@@ -9,6 +9,7 @@ from keywords.cloud_platform.dcmanager.dcmanager_subcloud_backup_keywords import
 from keywords.cloud_platform.dcmanager.dcmanager_subcloud_group_keywords import DcmanagerSubcloudGroupKeywords
 from keywords.cloud_platform.dcmanager.dcmanager_subcloud_list_keywords import DcManagerSubcloudListKeywords
 from keywords.cloud_platform.dcmanager.dcmanager_subcloud_manager_keywords import DcManagerSubcloudManagerKeywords
+from keywords.cloud_platform.dcmanager.dcmanager_subcloud_prestage import DcmanagerSubcloudPrestage
 from keywords.cloud_platform.dcmanager.dcmanager_subcloud_update_keywords import DcManagerSubcloudUpdateKeywords
 from keywords.cloud_platform.dcmanager.objects.dcmanager_subcloud_list_object_filter import DcManagerSubcloudListObjectFilter
 from keywords.cloud_platform.health.health_keywords import HealthKeywords
@@ -1237,3 +1238,76 @@ def test_verify_backup_restore_local_simplex_images(request):
 
     img_check = DockerImagesKeywords(subcloud_ssh).exists_image(local_registry, docker_img, docker_tag)
     validate_equals(img_check, True, f"Validate that {docker_img} was restored with backup.")
+
+
+@mark.p2
+@mark.lab_has_subcloud
+def test_restore_remote_backup_prestaged_subcloud_inactive_load(request):
+    """
+    Verify subcloud backup restore from a backup stored in
+    the subcloud triggered by controller-0. Subcloud must
+    be prestaged and running an inactive load.
+
+    Test Steps:
+        - Prestage the subcloud with inactive load
+        - Create a Subcloud backup and check it on central path
+        - Restore the subcloud central backup
+    Teardown:
+        - Remove files created while the Tc was running.
+    """
+    central_ssh = LabConnectionKeywords().get_active_controller_ssh()
+    subcloud_release = CloudPlatformVersionManagerClass().get_last_major_release()
+    get_logger().log_info(f"Subcloud release {subcloud_release}")
+
+    # Gets the lowest subcloud (the subcloud with the lowest id).
+    dcmanager_subcloud_list_keywords = DcManagerSubcloudListKeywords(central_ssh)
+    lowest_managed_subcloud = dcmanager_subcloud_list_keywords.get_one_subcloud_by_release(str(subcloud_release))
+    subcloud_name = lowest_managed_subcloud.get_name()
+
+    subcloud_ssh = LabConnectionKeywords().get_subcloud_ssh(subcloud_name)
+
+    # Prechecks Before Prestage
+    get_logger().log_info(f"Performing pre-checks on {subcloud_name}")
+    obj_health = HealthKeywords(subcloud_ssh)
+    obj_health.validate_healty_cluster()  # Checks alarms, pods, app health
+
+    # Gets the lowest subcloud sysadmin password needed for prestage, backup creation and deletion on central_path.
+    lab_config = ConfigurationManager.get_lab_config().get_subcloud(subcloud_name)
+    subcloud_password = lab_config.get_admin_credentials().get_password()
+
+    # Prestage subcloud
+    get_logger().log_info(f"Subcloud selected for prestage: {subcloud_name}")
+    DcmanagerSubcloudPrestage(central_ssh).dcmanager_subcloud_prestage(subcloud_name, subcloud_password)
+
+    # validate prestage status
+    obj_subcloud = dcmanager_subcloud_list_keywords.get_dcmanager_subcloud_list().get_subcloud_by_name(subcloud_name)
+    # Verify that the prestage is completed successfully
+    validate_equals(obj_subcloud.get_prestage_status(), "complete", f"subcloud {subcloud_name} successfully.")
+
+    # validate Healthy status
+    obj_health.validate_healty_cluster()
+
+    dc_manager_backup = DcManagerSubcloudBackupKeywords(central_ssh)
+
+    # Path to where the backup file will store.
+    central_path = "/opt/dc-vault/backups/"
+
+    def teardown():
+        get_logger().log_info(f"Managing subcloud {subcloud_name}")
+        DcManagerSubcloudManagerKeywords(central_ssh).get_dcmanager_subcloud_manage(subcloud_name, 10)
+
+        get_logger().log_info("Removing test files during teardown")
+        FileKeywords(subcloud_ssh).delete_folder_with_sudo(central_path)
+
+    request.addfinalizer(teardown)
+
+    # Create a subcloud backup on central
+    get_logger().log_info(f"Create {subcloud_name} backup on central")
+    dc_manager_backup.create_subcloud_backup(subcloud_password, central_ssh, path=f"{central_path}/{subcloud_name}/{subcloud_release}", subcloud=subcloud_name)
+
+    get_logger().log_info(f"Checking if backup was created on {subcloud_name}")
+    DcManagerSubcloudBackupKeywords(central_ssh).wait_for_backup_status_complete(subcloud_name, expected_status="complete-central")
+
+    DcManagerSubcloudManagerKeywords(central_ssh).get_dcmanager_subcloud_unmanage(subcloud_name, 10)
+    # Restore subcloud remote backup
+    dc_manager_backup.restore_subcloud_backup(subcloud_password, central_ssh, subcloud=subcloud_name, release=subcloud_release, with_install=True)
