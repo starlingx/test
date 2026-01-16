4e03e02069ca06e3d8c2e24bda087e279cbbc920
Suhailath Valavil <suhailath.valavil@windriver.com>
2026-01-16 21:32:31 +0000

    Revert "Add test to check kube-system pods, service and deployment"
    This reverts commit f7b2c6f351fb2f1d710289df00d74fe1dd7257a6.
    
    Reason for revert: Unit tests failed
    
    Change-Id: I9012f26f62b2a5d075d46f2d8a938f801fec249c
    Signed-off-by: Suhailath Valavil <suhailath.valavil@windriver.com>

--- keywords/k8s/deployments/kubectl_get_deployments_keywords.py
+++ keywords/k8s/deployments/kubectl_get_deployments_keywords.py
@@ -1,54 +1,49 @@
-from framework.ssh.ssh_connection import SSHConnection
 from keywords.base_keyword import BaseKeyword
-from keywords.k8s.deployments.object.kubectl_get_deployments_output import KubectlGetDeploymentOutput
 from keywords.k8s.k8s_command_wrapper import export_k8s_config
-
+from keywords.k8s.deployments.object.kubectl_get_deployments_output import KubectlGetDeploymentOutput
 
 class KubectlGetDeploymentsKeywords(BaseKeyword):
     """
     Class for Expose Deployment Keywords
     """
 
-    def __init__(self, ssh_connection: SSHConnection):
+    def __init__(self, ssh_connection):
         """
-        Constructor.
-
+        Constructor
         Args:
-            ssh_connection (SSHConnection): SSH connection object.
+            ssh_connection:
         """
         self.ssh_connection = ssh_connection
 
-    def get_deployment(self, deployment_name: str, namespace: str = None) -> KubectlGetDeploymentOutput:
+    def get_deployment(self, deployment_name: str):
         """
-        Get the deployments from the specified name.
+        Get the deployments from the specified name
+        Args:
+            deployment_name (str): the deployment name
 
+        Returns:
+            A list
+
+        """
+        kubectl_get_deployments_output = self.ssh_connection.send(export_k8s_config(f"kubectl get deployment {deployment_name}"))
+        self.validate_success_return_code(self.ssh_connection)
+        deployments_list_output = KubectlGetDeploymentOutput(kubectl_get_deployments_output)
+
+        return deployments_list_output
+    
+
+    def get_deployments(self, namespace: str = None):
+        """
+        Get the deployments from the specified namespace, or all namespaces if not specified.
         Args:
             deployment_name (str): the deployment name
             namespace (str): the namespace
 
         Returns:
-            KubectlGetDeploymentOutput: The deployment output object.
+            A list
+
         """
-        cmd = f"kubectl get deployment {deployment_name}"
-        if namespace:
-            cmd = f"{cmd} -n {namespace}"
-        kubectl_get_deployments_output = self.ssh_connection.send(export_k8s_config(cmd))
-        self.validate_success_return_code(self.ssh_connection)
-        deployments_list_output = KubectlGetDeploymentOutput(kubectl_get_deployments_output)
-
-        return deployments_list_output
-
-    def get_deployments(self, namespace: str = None) -> KubectlGetDeploymentOutput:
-        """
-        Get the deployments from the specified namespace, or all namespaces if not specified.
-
-        Args:
-            namespace (str): the namespace
-
-        Returns:
-            KubectlGetDeploymentOutput: The deployment output object.
-        """
-        cmd = "kubectl get deployment"
+        cmd = f"kubectl get deployment"
         if namespace:
             cmd = f"{cmd} -n {namespace}"
         kubectl_get_deployments_output = self.ssh_connection.send(export_k8s_config(cmd))
--- keywords/k8s/service/kubectl_get_service_keywords.py
+++ keywords/k8s/service/kubectl_get_service_keywords.py
@@ -1,4 +1,3 @@
-from framework.ssh.ssh_connection import SSHConnection
 from keywords.base_keyword import BaseKeyword
 from keywords.k8s.k8s_command_wrapper import export_k8s_config
 from keywords.k8s.service.object.kubectl_get_service_output import KubectlGetServicesOutput
@@ -10,46 +9,39 @@ class KubectlGetServiceKeywords(BaseKeyword):
     Keyword class for get service
     """
 
-    def __init__(self, ssh_connection: SSHConnection):
+    def __init__(self, ssh_connection):
         """
-        Constructor.
-
+        Constructor
         Args:
-            ssh_connection (SSHConnection): SSH connection object.
+            ssh_connection:
         """
         self.ssh_connection = ssh_connection
 
-    def get_service(self, service_name: str, namespace: str = None) -> KubectlServicesObject:
+    def get_service(self, service_name: str) -> KubectlServicesObject:
         """
-        Gets the service.
-
+        Gets the service
         Args:
-            service_name (str): the service name
-            namespace (str): the namespace
+            service_name (): the service name
 
-        Returns:
-            KubectlServicesObject: The service object.
+        Returns: KubectlServiceObject
+
         """
-        cmd = f"kubectl get service {service_name}"
-        if namespace:
-            cmd = f"{cmd} -n {namespace}"
-        kubectl_get_service_output = self.ssh_connection.send(export_k8s_config(cmd))
+        kubectl_get_service_output = self.ssh_connection.send(export_k8s_config(f"kubectl get service {service_name}"))
         self.validate_success_return_code(self.ssh_connection)
         service_list_output = KubectlGetServicesOutput(kubectl_get_service_output)
 
         return service_list_output.get_service(service_name)
-
+    
     def get_services(self, namespace: str = None) -> KubectlServicesObject:
         """
-        Gets the services from a namespace.
-
+        Gets the services from a namespace
         Args:
-            namespace (str): the namespace
+            namespace (): the namespace
 
-        Returns:
-            KubectlServicesObject: The services output object.
+        Returns: KubectlServiceObject
+
         """
-        cmd = "kubectl get services"
+        cmd = f"kubectl get services"
         if namespace:
             cmd = f"{cmd} -n {namespace}"
         kubectl_get_services_output = self.ssh_connection.send(export_k8s_config(cmd))
@@ -57,18 +49,10 @@ class KubectlGetServiceKeywords(BaseKeyword):
         services_list_output = KubectlGetServicesOutput(kubectl_get_services_output)
         return services_list_output
 
-    def get_service_node_port(self, service_name: str) -> str:
-        """
-        Gets the node port for a service.
 
-        Args:
-            service_name (str): the service name
-
-        Returns:
-            str: The node port value.
-        """
+    def get_service_node_port(self, service_name) -> str:
         jsonpath = 'jsonpath="{.spec.ports[0].nodePort}"'
-        node_port = self.ssh_connection.send(export_k8s_config(f"kubectl get service {service_name} -o {jsonpath}"))
+        node_port = self.ssh_connection.send(export_k8s_config(f'kubectl get service {service_name} -o {jsonpath}'))
         self.validate_success_return_code(self.ssh_connection)
         if len(node_port) > 0:
             return node_port[0]
