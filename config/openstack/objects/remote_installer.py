4635bac2d63164ebe587ec3b23eb8c8164f6f478
Soraganvi <Anand.Soraganvi@windriver.com>
2026-02-25 10:40:02 -0500

    Aded new config feilds for remote installer
    Change-Id: I4d0fb95583227bcbb52d48e429f0bd45d7ea1671
    Signed-off-by: Soraganvi <Anand.Soraganvi@windriver.com>

--- config/openstack/objects/remote_installer.py
+++ config/openstack/objects/remote_installer.py
@@ -4,14 +4,17 @@ class RemoteInstaller:
     """
 
     def __init__(self, remote_dict: []):
-        self.enabled_flag = remote_dict['enabled']
-        self.file_server = remote_dict['file_server']
-        self.app_version = remote_dict['app_version']
-        self.app_build = remote_dict['app_build']
+        self.enabled_flag = remote_dict["enabled"]
+        self.file_server = remote_dict["file_server"]
+        self.app_version = remote_dict["app_version"]
+        self.app_build = remote_dict["app_build"]
+        self.file_location = remote_dict.get("file_location", "/home/sysadmin/")
+        self.tar_file_path = remote_dict.get("tar_file_path", "")
 
     def get_enabled_flag(self) -> str:
         """
-        Getter for remote enabled flag
+        Getter for remote enabled flag.
+
         Returns: the enabled_flag
 
         """
@@ -19,15 +22,17 @@ class RemoteInstaller:
 
     def get_file_server(self) -> str:
         """
-        Getter for file server
+        Getter for file server.
+
         Returns: the file_server
 
         """
         return self.file_server
 
-    def get_app_version(self) :
+    def get_app_version(self):
         """
-        Getter for application version
+        Getter for application version.
+
         Returns: the app_version
 
         """
@@ -35,15 +40,35 @@ class RemoteInstaller:
 
     def get_app_build(self) -> str:
         """
-        Getter for application build
+        Getter for application build.
+
         Returns: the app_build
 
         """
         return self.app_build
 
+    def get_file_location(self) -> str:
+        """
+        Getter for file location.
+
+        Returns: the file_location
+
+        """
+        return self.file_location
+
+    def get_tar_file_path(self) -> str:
+        """
+        Getter for tar file path.
+
+        Returns: the tar_file_path
+
+        """
+        return self.tar_file_path
+
     def get_file_path(self) -> str:
         """
-        This function will return a single string representation of the installer location remote object
+        This function will return a single string representation of the installer location remote object.
+
         Returns: str
 
         """
