6c33870be0c680fea0dcaf79b1084f87d223b344
afreitas <Ana.FreitasdasChagas@windriver.com>
2026-03-23 16:58:57 -0300

    Add system_modify_latitude_longitude to SystemModifyKeywords
    Change-Id: Ic12ad7e4b05a58c68d45fb5c554f2b1ed855134f
    Signed-off-by: afreitas <Ana.FreitasdasChagas@windriver.com>

--- keywords/cloud_platform/system/modify/system_modify_keywords.py
+++ keywords/cloud_platform/system/modify/system_modify_keywords.py
@@ -6,18 +6,24 @@ from keywords.cloud_platform.system.show.objects.system_show_output import Syste
 
 
 class SystemModifyKeywords(BaseKeyword):
+    """Keywords for the 'system modify' CLI command."""
 
     def __init__(self, ssh_connection):
-        """
-        Constructor
+        """Constructor.
+
         Args:
-            ssh_connection:
+            ssh_connection: SSH connection to the target host.
         """
         self.ssh_connection = ssh_connection
 
     def system_modify_timezone(self, timezone: str) -> SystemShowOutput:
-        """
-        Runs system command system modify --timezone
+        """Run system modify --timezone.
+
+        Args:
+            timezone (str): Timezone value to set.
+
+        Returns:
+            SystemShowOutput: The output of the system modify command.
         """
         output = self.ssh_connection.send(source_openrc(f'system modify --timezone="{timezone}"'))
         self.validate_success_return_code(self.ssh_connection)
@@ -31,16 +37,28 @@ class SystemModifyKeywords(BaseKeyword):
         return system_show_output
 
     def system_modify_https_enabled(self, https_enabled: bool) -> SystemShowOutput:
-        """
-        Runs system command system modify --https-enabled True
-        alias system modify -p True
+        """Run system modify --https-enabled.
 
         Args:
-            https_enabled: boolean value to enable or disable https
+            https_enabled (bool): Boolean value to enable or disable https.
 
         Returns:
-            SystemShowOutput object
+            SystemShowOutput: The output of the system modify command.
         """
         output = self.ssh_connection.send(source_openrc(f"system modify -p {https_enabled}"))
+        self.validate_success_return_code(self.ssh_connection)
+        return SystemShowOutput(output)
+
+    def system_modify_latitude_longitude(self, latitude: float, longitude: float) -> SystemShowOutput:
+        """Run system modify --latitude <lat> --longitude <long>.
+
+        Args:
+            latitude (float): Latitude value to set.
+            longitude (float): Longitude value to set.
+
+        Returns:
+            SystemShowOutput: The output of the system modify command.
+        """
+        output = self.ssh_connection.send(source_openrc(f"system modify --latitude {latitude} --longitude {longitude}"))
         self.validate_success_return_code(self.ssh_connection)
         return SystemShowOutput(output)
