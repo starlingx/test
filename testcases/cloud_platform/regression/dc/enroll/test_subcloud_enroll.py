e1b474619e575b1010e0faea34ab172333a6efee
Gustavo Pereira <gustavo.lyrapereira@windriver.com>
2026-03-30 13:43:28 -0300

    Enroll testcases improvement
    Improve test cases to retrieve the subcloud from config
    file and verify that the selected subcloud was factory
    installed before proceeding with enrollment.
    
    Change-Id: I9b055599252d35329b3d8b55dc771d0e951be566
    Signed-off-by: Gustavo Pereira <gustavo.lyrapereira@windriver.com>

--- testcases/cloud_platform/regression/dc/enroll/test_subcloud_enroll.py
+++ testcases/cloud_platform/regression/dc/enroll/test_subcloud_enroll.py
@@ -10,7 +10,6 @@ from keywords.cloud_platform.dcmanager.dcmanager_subcloud_delete_keywords import
 from keywords.cloud_platform.dcmanager.dcmanager_subcloud_deploy_keywords import DCManagerSubcloudDeployKeywords
 from keywords.cloud_platform.dcmanager.dcmanager_subcloud_list_keywords import DcManagerSubcloudListKeywords
 from keywords.cloud_platform.dcmanager.dcmanager_subcloud_manager_keywords import DcManagerSubcloudManagerKeywords
-from keywords.cloud_platform.dcmanager.objects.dcmanager_subcloud_list_object import DcManagerSubcloudListObject
 from keywords.cloud_platform.ssh.lab_connection_keywords import LabConnectionKeywords
 from keywords.files.file_keywords import FileKeywords
 
@@ -59,28 +58,6 @@ def create_fake_bootstrap_values(bootstrap_values: str, ssh_connection: SSHConne
 
     return fake_bootstrap_values
 
-def remove_deployed_subcloud(ssh_connection: SSHConnection, lowest_subcloud_obj: DcManagerSubcloudListObject):
-    """Power off, unmanage, and delete a deployed subcloud.
-
-    Args:
-        ssh_connection: SSH connection to the system controller.
-        lowest_subcloud_obj: Subcloud object to remove.
-    """
-    subcloud_name = lowest_subcloud_obj.get_name()
-    dcm_sc_manager_kw = DcManagerSubcloudManagerKeywords(ssh_connection)
-    # poweroff the subcloud.
-    get_logger().log_test_case_step(f"Poweroff subcloud={subcloud_name}.")
-    dcm_sc_manager_kw.set_subcloud_poweroff(subcloud_name)
-    # Unmanage the subcloud.
-    if lowest_subcloud_obj.get_management() == "managed":
-        get_logger().log_test_case_step(f"Unmanage subcloud={subcloud_name}.")
-        dcm_sc_manage_output = dcm_sc_manager_kw.get_dcmanager_subcloud_unmanage(subcloud_name, timeout=10)
-        get_logger().log_info(
-            f"The management state of the subcloud {subcloud_name} was changed to {dcm_sc_manage_output.get_dcmanager_subcloud_manage_object().get_management()}.")
-    # delete the subcloud.
-    dcm_sc_del_kw = DcManagerSubcloudDeleteKeywords(ssh_connection)
-    dcm_sc_del_kw.dcmanager_subcloud_delete(subcloud_name)
-
 def run_enroll_operations(
         system_controller_ssh: SSHConnection,
         subcloud_name: str = None,
@@ -126,7 +103,7 @@ def test_enroll_one_sx_subcloud():
     Execute a subcloud enroll
 
     Test Steps:
-        - Shutdown and delete a subcloud if no undeployed
+        - Validate the selected subcloud is factory-installed.
           subclouds are found.
         - Run enroll command.
 
@@ -135,15 +112,11 @@ def test_enroll_one_sx_subcloud():
     deployment_assets_config = ConfigurationManager.get_deployment_assets_config()
     system_controller_ssh = LabConnectionKeywords().get_active_controller_ssh()
 
-    dcm_sc_list_kw = DcManagerSubcloudListKeywords(system_controller_ssh)
-    subcloud_name = dcm_sc_list_kw.get_dcmanager_subcloud_list().get_undeployed_subcloud_name()
-
-    if subcloud_name is None:
-
-        get_logger().log_info("No Undeployed Subcloud found deleting existing one")
-        selected_subcloud_obj = dcm_sc_list_kw.get_dcmanager_subcloud_list().get_specific_subcloud_with_lowest_id(subcloud_type="simplex")
-        remove_deployed_subcloud(system_controller_ssh, lowest_subcloud_obj=selected_subcloud_obj)
-        subcloud_name = selected_subcloud_obj.get_name()
+    subcloud_name = ConfigurationManager.get_lab_config().get_subcloud_names()[0]
+    subcloud_ssh = LabConnectionKeywords().get_subcloud_ssh(subcloud_name)
+    get_logger().log_test_case_step(f"Verify that {subcloud_name} has completed factory-install process.")
+    if not FileKeywords(subcloud_ssh).file_exists("/var/lib/factory-install/complete"):
+        fail("This subcloud was not factory installed, unable to proceed with enroll.")
 
     bootstrap_values = deployment_assets_config.get_subcloud_deployment_assets(subcloud_name).get_bootstrap_file()
     install_values = deployment_assets_config.get_subcloud_deployment_assets(subcloud_name).get_install_file()
@@ -161,8 +134,7 @@ def test_retry_enroll_one_sx_subcloud(request):
     data. run subcloud enrollment retry.
 
     Test Steps:
-        - Shutdown and delete a subcloud if no undeployed
-          subclouds are found.
+        - Validate the selected subcloud is factory-installed.
         - Run enroll command passing invalid dm file with wrong data.
         - verify that enrollment failed.
         - Run subcloud enrollment retry.
@@ -172,14 +144,12 @@ def test_retry_enroll_one_sx_subcloud(request):
     deployment_assets_config = ConfigurationManager.get_deployment_assets_config()
     system_controller_ssh = LabConnectionKeywords().get_active_controller_ssh()
 
-    dcm_sc_list_kw = DcManagerSubcloudListKeywords(system_controller_ssh)
-    subcloud_name = dcm_sc_list_kw.get_dcmanager_subcloud_list().get_undeployed_subcloud_name()
+    subcloud_name = ConfigurationManager.get_lab_config().get_subcloud_names()[0]
+    subcloud_ssh = LabConnectionKeywords().get_subcloud_ssh(subcloud_name)
 
-    if subcloud_name is None:
-        get_logger().log_info("No Undeployed Subcloud found deleting existing one")
-        selected_subcloud_obj = dcm_sc_list_kw.get_dcmanager_subcloud_list().get_specific_subcloud_with_lowest_id(subcloud_type="simplex")
-        remove_deployed_subcloud(system_controller_ssh, lowest_subcloud_obj=selected_subcloud_obj)
-        subcloud_name = selected_subcloud_obj.get_name()
+    get_logger().log_test_case_step(f"Verify that {subcloud_name} has completed factory-install process.")
+    if not FileKeywords(subcloud_ssh).file_exists("/var/lib/factory-install/complete"):
+        fail("This subcloud was not factory installed, unable to proceed with enroll.")
 
     bootstrap_values = deployment_assets_config.get_subcloud_deployment_assets(subcloud_name).get_bootstrap_file()
     install_values = deployment_assets_config.get_subcloud_deployment_assets(subcloud_name).get_install_file()
@@ -212,30 +182,26 @@ def test_enroll_sx_subcloud_after_factory_restore(request):
     Test subcloud enroll after a factory restore.
 
     Test Steps:
-        - Delete a deployed subcloud if no undeployed subclouds are found.
+        - Validate the selected subcloud is factory-installed.
         - Create a deploy config yml file.
         - Create a subcloud backup.
         - Restore the subcloud to factory state.
         - Enroll the subcloud.
         - Validate enroll completion and availability.
     """
-    deployment_assets_config = ConfigurationManager.get_deployment_assets_config()
     system_controller_ssh = LabConnectionKeywords().get_active_controller_ssh()
 
     dcm_sc_list_kw = DcManagerSubcloudListKeywords(system_controller_ssh)
     dc_manager_backup = DcManagerSubcloudBackupKeywords(system_controller_ssh)
+    subcloud_name = ConfigurationManager.get_lab_config().get_subcloud_names()[0]
+    subcloud_ssh = LabConnectionKeywords().get_subcloud_ssh(subcloud_name)
 
-    subcloud_name = dcm_sc_list_kw.get_dcmanager_subcloud_list().get_undeployed_subcloud_name()
+    get_logger().log_test_case_step(f"Verify that {subcloud_name} has completed factory-install process.")
+    if not FileKeywords(subcloud_ssh).file_exists("/var/lib/factory-install/complete"):
+        fail("This subcloud was not factory installed, unable to proceed with enroll.")
 
     lab_config = ConfigurationManager.get_lab_config().get_subcloud(subcloud_name)
     subcloud_password = lab_config.get_admin_credentials().get_password()
-
-    if subcloud_name is None:
-        get_logger().log_info("No Undeployed Subcloud found deleting existing one")
-        selected_subcloud_obj = dcm_sc_list_kw.get_dcmanager_subcloud_list().get_specific_subcloud_with_lowest_id(subcloud_type="simplex")
-        remove_deployed_subcloud(system_controller_ssh, lowest_subcloud_obj=selected_subcloud_obj)
-        subcloud_name = selected_subcloud_obj.get_name()
-
     get_logger().log_test_case_step(f"Creating subcloud {subcloud_name}.")
     DCManagerSubcloudDeployKeywords(system_controller_ssh).dcmanager_subcloud_deploy_create(subcloud_name)
 
